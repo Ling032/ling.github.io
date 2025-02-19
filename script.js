9c424b390f27c0ccb338b75e4d17ba6390d7feef
Ling032 <jundeling0322@gmail.com>
2025-02-19 16:41:25 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+window.onload = function() {
+    // Show a scary alert after 3 seconds
+    setTimeout(function() {
+      alert("Do you hear something?");
+    }, 3000);
+    
+    // You can add more horror effects here
+    // For instance, random jump-scare popups or screen flicker
+  };
+  
